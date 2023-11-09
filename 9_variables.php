59f25e9a87d136c150b4596b716a5f20a423e2c8
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-09 10:06:11 +0700

    INIT

---
+++ 9_variables.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$name = 'Thanh';
+$number = 100;
+$Number_List = 100.5;
+
+$NUMBer = 1000;
+
+echo $name . " " . $NUMBer;
+echo '<br/><br/>';
+echo $number;
+echo '<hr/>';
+echo '<br/><br/>';
+echo $Number_List;
+
+
+?>
+
+
+</body>
+</html>
